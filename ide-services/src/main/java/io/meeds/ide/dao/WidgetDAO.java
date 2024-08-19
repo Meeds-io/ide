321a4eda6f23e9ac30db8d5fa8ba30fe5285caa6
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-08-19 18:14:45 +0100

    feat: Implement IDE Backend and FrontEnd Base - MEED-7330 - Meeds-io/MIPs#153 (#1)

--- ide-services/src/main/java/io/meeds/ide/dao/WidgetDAO.java
+++ ide-services/src/main/java/io/meeds/ide/dao/WidgetDAO.java
@@ -1,36 +1,31 @@
-/*
+/**
  * This file is part of the Meeds project (https://meeds.io/).
- * 
+ *
  * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+package io.meeds.ide.dao;
 
-const path = require('path');
-const { merge } = require('webpack-merge');
-const webpackCommonConfig = require('./webpack.prod.js');
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// the display name of the war
-const app = 'ide';
+import io.meeds.ide.entity.WidgetEntity;
 
-const exoServerPath = "/exo-server";
+@Repository
+public interface WidgetDAO extends JpaRepository<WidgetEntity, Long> {
 
-let config = merge(webpackCommonConfig, {
-  output: {
-    path: path.resolve(`${exoServerPath}/webapps/${app}/`)
-  },
-  mode: 'development',
-  devtool: 'eval-source-map'
-});
-module.exports = config;
+  boolean existsByPortletId(Long id);
+
+}
