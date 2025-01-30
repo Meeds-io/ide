d4105d6876a4354eb4b3eb4e1525c460fe7abdbb
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-01-30 12:14:52 +0100

    feat: Add static CSS/JS apps to a dedicated site - MEED-8148 - Meeds-io/MIPs#167 (#11)
    This PR will implement a new feature that will allow the admin to add
    js/css applications to a dedicated site via a new drawer component

---
+++ ide-service/src/test/java/io/meeds/ide/rest/StaticResourceApplicationRestTest.java
@@ -0,0 +1,173 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package io.meeds.ide.rest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.meeds.ide.service.StaticResourceApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import io.meeds.ide.model.Widget;
+import io.meeds.spring.web.security.PortalAuthenticationManager;
+import io.meeds.spring.web.security.WebSecurityConfiguration;
+
+import jakarta.servlet.Filter;
+import lombok.SneakyThrows;
+
+@SpringBootTest(classes = { StaticResourceApplicationRest.class, PortalAuthenticationManager.class, })
+@ContextConfiguration(classes = { WebSecurityConfiguration.class })
+@AutoConfigureWebMvc
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class StaticResourceApplicationRestTest {
+
+  private static final String REST_PATH     = "/static/resources"; // NOSONAR
+
+  private static final String SIMPLE_USER   = "simple";
+
+  private static final String TEST_PASSWORD = "testPassword";
+
+  static final ObjectMapper   OBJECT_MAPPER;
+
+  static {
+    // Workaround when Jackson is defined in shared library with different
+    // version and without artifact jackson-datatype-jsr310
+    OBJECT_MAPPER = JsonMapper.builder()
+                              .configure(JsonReadFeature.ALLOW_MISSING_VALUES, true)
+                              .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                              .build();
+    OBJECT_MAPPER.registerModule(new JavaTimeModule());
+  }
+
+  @MockBean
+  private StaticResourceApplicationService staticResourceApplicationService;
+
+  @Autowired
+  private SecurityFilterChain              filterChain;
+
+  @Autowired
+  private WebApplicationContext            context;
+
+  @Mock
+  private Widget                           widget;
+
+  private MockMvc                          mockMvc;
+
+  @BeforeEach
+  void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).addFilters(filterChain.getFilters().toArray(new Filter[0])).build();
+  }
+
+  RequestPostProcessor testSimpleUser() {
+    return user(SIMPLE_USER).password(TEST_PASSWORD).authorities(new SimpleGrantedAuthority("users"));
+  }
+
+  RequestPostProcessor testAdministrator() {
+    return user(SIMPLE_USER).password(TEST_PASSWORD).authorities(new SimpleGrantedAuthority("administrators"));
+  }
+
+  @Test
+  void getStaticResourceApplicationsAnonymously() throws Exception {
+    ResultActions response = mockMvc.perform(get(REST_PATH).param("siteName", "siteName"));
+    response.andExpect(status().isForbidden());
+  }
+
+  @Test
+  void getStaticResourceApplicationsWithUser() throws Exception {
+    ResultActions response = mockMvc.perform(get(REST_PATH).param("siteName", "siteName").with(testSimpleUser()));
+    response.andExpect(status().isForbidden());
+  }
+
+  @Test
+  void getStaticResourceApplicationsWithAdministrator() throws Exception {
+    ResultActions response = mockMvc.perform(get(REST_PATH).param("siteName", "siteName").with(testAdministrator()));
+    response.andExpect(status().isOk());
+
+    doThrow(new IllegalAccessException()).when(staticResourceApplicationService)
+                                         .getStaticResourceApplications("siteName", SIMPLE_USER);
+
+    response = mockMvc.perform(get(REST_PATH).param("siteName", "siteName").with(testAdministrator()));
+    response.andExpect(status().isForbidden());
+  }
+
+  @Test
+  void createStaticResourceApplicationAnonymously() throws Exception {
+    ResultActions response =
+                           mockMvc.perform(post(REST_PATH).content(asJsonString(widget)).contentType(MediaType.APPLICATION_JSON));
+    response.andExpect(status().isForbidden());
+  }
+
+  @Test
+  void createStaticResourceApplicationWithUser() throws Exception {
+    ResultActions response = mockMvc.perform(post(REST_PATH).with(testSimpleUser())
+                                                            .content(asJsonString(widget))
+                                                            .contentType(MediaType.APPLICATION_JSON));
+    response.andExpect(status().isForbidden());
+  }
+
+  @Test
+  void createStaticResourceApplicationWithAdministrator() throws Exception {
+    ResultActions response = mockMvc.perform(post(REST_PATH).with(testAdministrator())
+                                                            .content(asJsonString(widget))
+                                                            .contentType(MediaType.APPLICATION_JSON));
+    response.andExpect(status().isOk());
+
+    doThrow(new IllegalAccessException()).when(staticResourceApplicationService)
+                                         .createStaticResourceApplication(any(Widget.class), anyString());
+
+    response = mockMvc.perform(post(REST_PATH).with(testAdministrator())
+                                              .content(asJsonString(widget))
+                                              .contentType(MediaType.APPLICATION_JSON));
+    response.andExpect(status().isForbidden());
+  }
+
+  @SneakyThrows
+  String asJsonString(final Object obj) {
+    return OBJECT_MAPPER.writeValueAsString(obj);
+  }
+
+}
