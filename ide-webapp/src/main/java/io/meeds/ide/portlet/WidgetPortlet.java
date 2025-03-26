91560e42ca69ffee6e89ff8df320e5b79b8467f7
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-03-26 10:40:45 +0100

    feat: Fix IDE Gadget creation - MEED-8660 - Meeds-io/MIPs#180 (#34)
    This PR will revert work done for WidgetPortletInstancePreferencePlugin.

--- ide-webapp/src/main/java/io/meeds/ide/portlet/WidgetPortlet.java
+++ ide-webapp/src/main/java/io/meeds/ide/portlet/WidgetPortlet.java
@@ -1,4 +1,4 @@
-/*
+/**
  * This file is part of the Meeds project (https://meeds.io/).
  *
  * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
@@ -19,8 +19,6 @@
 package io.meeds.ide.portlet;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -31,7 +29,7 @@ import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import io.meeds.social.util.JsonUtils;
+import io.meeds.social.cms.service.CMSService;
 import org.exoplatform.commons.ObjectAlreadyExistsException;
 import org.exoplatform.commons.api.portlet.GenericDispatchedViewPortlet;
 import org.exoplatform.container.ExoContainerContext;
@@ -45,9 +43,6 @@ import org.exoplatform.social.webui.Utils;
 
 import io.meeds.ide.model.Widget;
 import io.meeds.ide.service.WidgetService;
-
-import static io.meeds.layout.util.JsonUtils.fromJsonString;
-import static io.meeds.social.portlet.CMSPortlet.DATA_INIT_PREFERENCE_NAME;
 
 public class WidgetPortlet extends GenericDispatchedViewPortlet {
 
@@ -87,7 +82,7 @@ public class WidgetPortlet extends GenericDispatchedViewPortlet {
 
   private void checkPreferences(RenderRequest request) throws PortletException {
     PortletPreferences preferences = request.getPreferences();
-    if (preferences.getValue(PORTLET_INSTANCE_ID_PARAM, null) != null) {
+    if (preferences.getValue(PORTLET_INSTANCE_ID_PARAM, null) != null && preferences.getValue(WIDGET_ID_PARAM, null) == null) {
       long portletInstanceId = Long.parseLong(preferences.getValue(PORTLET_INSTANCE_ID_PARAM, null));
       Identity identity = Utils.getViewerIdentity();
       try {
@@ -96,10 +91,6 @@ public class WidgetPortlet extends GenericDispatchedViewPortlet {
         if (widget == null) {
           widget = new Widget();
           widget.setPortletId(portletInstanceId);
-          Widget imported = fromJsonString(preferences.getValue(DATA_INIT_PREFERENCE_NAME, null), Widget.class);
-          widget.setJs(imported.getJs());
-          widget.setHtml(imported.getHtml());
-          widget.setCss(imported.getCss());
           widget = widgetService.createWidget(widget, identity.getRemoteId());
         }
         preferences.setValue(WIDGET_ID_PARAM, String.valueOf(widget.getId()));
@@ -108,11 +99,6 @@ public class WidgetPortlet extends GenericDispatchedViewPortlet {
         ApplicationState state = applicationModel.getState();
         Portlet prefs = getLayoutService().load(state);
         prefs.setValue(WIDGET_ID_PARAM, String.valueOf(widget.getId()));
-        Map<String, String> content = new HashMap<>();
-        content.put("html", widget.getHtml());
-        content.put("js", widget.getJs());
-        content.put("css", widget.getCss());
-        prefs.setValue(DATA_INIT_PREFERENCE_NAME, JsonUtils.toJsonString(content));
         layoutService.save(state, prefs);
       } catch (IllegalAccessException e) {
         throw new PortletException("User not allowed to change Widget settings", e);
